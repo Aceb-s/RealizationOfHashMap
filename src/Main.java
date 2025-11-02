719990e3589c1a8f4af73070db1bf087316d6862
Aceb-s <07ilmir07@gmail.com>
2025-11-02 19:02:40 +0300

    deleted all to commit all back again

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,2 @@
 public class Main {
-    public static void main(String[] args) {
-        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
-
-        map.put("apple", 1);
-        map.put("banana", 2);
-        map.put("orange", 3);
-
-        System.out.println("После добавления: " + map);
-        {
-
-            System.out.println("apple: " + map.get("apple"));
-            System.out.println("banana: " + map.get("banana"));
-            System.out.println("unknown: " + map.get("unknown"));
-
-            map.put("apple", 10);
-            System.out.println("После обновления apple: " + map);
-
-            map.remove("banana");
-            System.out.println("После удаления banana: " + map);
-        }
-    }
 }
