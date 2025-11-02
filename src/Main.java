b338df60986a8f3b7bc2b438e8129bd941f4020c
Aceb-s <07ilmir07@gmail.com>
2025-11-02 18:41:52 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        map.put("apple", 1);
+        map.put("banana", 2);
+        map.put("orange", 3);
+
+        System.out.println("После добавления: " + map);
+        {
+
+            System.out.println("apple: " + map.get("apple"));
+            System.out.println("banana: " + map.get("banana"));
+            System.out.println("unknown: " + map.get("unknown"));
+
+            map.put("apple", 10);
+            System.out.println("После обновления apple: " + map);
+
+            map.remove("banana");
+            System.out.println("После удаления banana: " + map);
         }
     }
 }
